bf47d943049d70d89e447590b52e99c17d2ad1f3
SaNguyen <sa.nguyentl@gmail.com>
2021-09-03 08:02:54 -0500

    The first commit

---
+++ src/main/java/com/acme/bankapp/api/ScheduledPaymentsApiController.java
@@ -0,0 +1,24 @@
+package com.acme.bankapp.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-09-02T21:42:03.713950-05:00[America/Chicago]")
+@Controller
+@RequestMapping("${openapi.accountAndTransactionAPISpecification.base-path:/open-banking/v3.1/aisp}")
+public class ScheduledPaymentsApiController implements ScheduledPaymentsApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public ScheduledPaymentsApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
---
+++ src/main/java/com/acme/bankapp/model/OBReadDirectDebit2Data.java
@@ -0,0 +1,92 @@
+package com.acme.bankapp.model;
+
+import java.util.Objects;
+import com.acme.bankapp.model.OBReadDirectDebit2DataDirectDebit;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * OBReadDirectDebit2Data
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-09-02T21:42:03.713950-05:00[America/Chicago]")
+public class OBReadDirectDebit2Data   {
+  @JsonProperty("DirectDebit")
+  @Valid
+  private List<OBReadDirectDebit2DataDirectDebit> directDebit = null;
+
+  public OBReadDirectDebit2Data directDebit(List<OBReadDirectDebit2DataDirectDebit> directDebit) {
+    this.directDebit = directDebit;
+    return this;
+  }
+
+  public OBReadDirectDebit2Data addDirectDebitItem(OBReadDirectDebit2DataDirectDebit directDebitItem) {
+    if (this.directDebit == null) {
+      this.directDebit = new ArrayList<OBReadDirectDebit2DataDirectDebit>();
+    }
+    this.directDebit.add(directDebitItem);
+    return this;
+  }
+
+  /**
+   * Get directDebit
+   * @return directDebit
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public List<OBReadDirectDebit2DataDirectDebit> getDirectDebit() {
+    return directDebit;
+  }
+
+  public void setDirectDebit(List<OBReadDirectDebit2DataDirectDebit> directDebit) {
+    this.directDebit = directDebit;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    OBReadDirectDebit2Data obReadDirectDebit2Data = (OBReadDirectDebit2Data) o;
+    return Objects.equals(this.directDebit, obReadDirectDebit2Data.directDebit);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(directDebit);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class OBReadDirectDebit2Data {\n");
+    
+    sb.append("    directDebit: ").append(toIndentedString(directDebit)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
